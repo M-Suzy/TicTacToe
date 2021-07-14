a41d839afb7e33834560950bc6d773916daff93d
grigyan <grigornalbandyan26@gmail.com>
2021-07-14 23:22:53 +0400

    add Player class

---
+++ src/Player.java
@@ -0,0 +1,37 @@
+public class Player {
+    private String name;
+    private char shape; // X or O
+    private int score;
+    //static Board board;
+    //we need static board, so that when calling makeMove() method we change the board
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public char getShape() {
+        return shape;
+    }
+
+    public void setShape(char shape) {
+        this.shape = shape;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    // will change the board
+    public void makeMove(int i, int j) {
+
+    }
+
+}
